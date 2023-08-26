6a935f69498d552a8e3d6002ef6fbe9a9f064de0
Aleksey <rav.92@list.ru>
2023-08-26 16:28:58 +0300

    add model ingredient

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,36 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(
+        max_length=settings.NAME_LENGTH,
+        verbose_name='Имя',
+        unique=True,
+        blank=False,
+    )
+    color = models.CharField(
+        max_length=settings.COLOR_LENGTH,
+        verbose_name='Код цвета',
+        unique=True,
+        blank=False,
+    )
+    slug = models.SlugField(
+        max_length=settings.NAME_LENGTH,
+        unique=True,
+        blank=False,
+    )
+    
+class Ingredient(models.Model):
+    name = models.CharField(
+        max_length=settings.NAME_LENGTH,
+        verbose_name='Название',
+        unique=True,
+        blank=False,
+    )
+    measurment_unit = models.CharField(
+        max_length=settings.NAME_LENGTH,
+        verbose_name='Единица измерения',
+        unique=True,
+        blank=False,
+    )
