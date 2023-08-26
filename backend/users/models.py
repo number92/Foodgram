29f57a7adff09f973874dc22e100e2136ec19456
Aleksey <rav.92@list.ru>
2023-08-26 15:54:25 +0300

    add users & recipes

---
+++ backend/users/models.py
@@ -0,0 +1,55 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from .validators import validate_username
+
+
+class User(AbstractUser):
+    """Класс пользователей."""
+    username = models.CharField(
+        max_length=settings.NAME_LENGTH,
+        verbose_name='Имя пользователя',
+        unique=True,
+        blank=False,
+        validators=[validate_username],
+        help_text=(f'Напишите ваше имя пользователя длиной'
+                   f'от {settings.MIN_USERNAME} '
+                   f'до {settings.MAX_USERNAME} символов')
+    )
+    email = models.EmailField(
+        max_length=settings.EMAIL_LENGTH,
+        verbose_name='Email',
+        unique=True,
+        blank=False
+    )
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ('username',)
+
+    def __str__(self):
+        return self.username
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(User,
+                             on_delete=models.CASCADE,
+                             related_name='follower',
+                             verbose_name='Подписчик')
+    following = models.ForeignKey(User,
+                                  on_delete=models.CASCADE,
+                                  related_name='following',
+                                  verbose_name='Подписаться')
+
+    def __str__(self) -> str:
+        return (f'{self.user}, {self.following.username}')
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'following'],
+                name='unique_user_following'
+            )
+        ]
---
+++ backend/users/validators.py
@@ -0,0 +1,21 @@
+import re
+
+from django.conf import settings
+from django.core.exceptions import ValidationError
+
+ERROR_MESSAGE = 'Имя пользователя содержит недопустимые символы: {}'
+
+
+def validate_username(value):
+    if (settings.MIN_USERNAME > len(value)
+       or settings.MAX_USERNAME > len(value)):
+        raise ValidationError(
+            f'допустимая длина от {settings.MIN_USERNAME} до'
+            f'"{settings.MAX_USERNAME}" символов.'
+        )
+    invalid_chars = set(re.findall(settings.EXCEPTION_CHARACTERS, value))
+    if invalid_chars:
+        raise ValidationError(
+            ERROR_MESSAGE.format(''.join(invalid_chars))
+        )
+    return value
